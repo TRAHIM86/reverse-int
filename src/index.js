c8eea0486132ba50b8820919cc3fe71e5a4b1792
TRAHIM86 <trahim1986@gmail.com>
2024-08-12 19:01:25 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverseInt(num) {
+    console.log(typeof num);
+    return Math.abs(num) 
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
 }
